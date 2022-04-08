1a837068c92ca546d7dc10e2f2a464a7942aa2b5
Zoran <mihov.zoran@hotmail.com>
2022-04-08 22:07:22 +0200

    Database fix

--- database/migrations/2022_01_22_130110_upload_files_table.php
+++ database/migrations/2022_01_22_130110_upload_files_table.php
@@ -15,12 +15,12 @@ class UploadFilesTable extends Migration
     {
         Schema::create('uploaded_files', function (Blueprint $table) {
             $table->id();
-            $table->string('description');
-            $table->string('name');
-            $table->string('type');
-            $table->json("shared_with");
-            $table->string('by_user');
-            $table->string('path');
+            $table->string('description')->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->string('type')->nullable(false);
+            $table->json("shared_with")->nullable(false);
+            $table->string('by_user')->nullable(false);
+            $table->string('path')->nullable(false);
             $table->timestamps();
         });
     }
